a191dc53200418205942c50712ccc945c9264a6e
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-07-27 00:21:44 -0700

    Change to a functional component

--- client/src/common/components/TopBar/index.js
+++ client/src/common/components/TopBar/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { Wrapper, LeftSection, MiddleSection, RightSection, Title } from './styled'
+import {
+  Wrapper,
+  LeftSection,
+  MiddleSection,
+  RightSection,
+  Title
+} from './styled'
 
-class TopBar extends React.Component {
-  render() {
-    return (
-      <Wrapper>
-        <LeftSection>
-          {this.props.LeftSection}
-        </LeftSection>
-        <MiddleSection>
-          <Title adjustsFontSizeToFit numberOfLines={1}>
-            {this.props.Title}
-          </Title>
-        </MiddleSection>
-        <RightSection>
-          {this.props.RightSection}
-        </RightSection>
-      </Wrapper>
-    )
-  }
+const TopBar = () => {
+  return (
+    <Wrapper>
+      <LeftSection>{this.props.LeftSection}</LeftSection>
+      <MiddleSection>
+        <Title adjustsFontSizeToFit numberOfLines={1}>
+          {this.props.Title}
+        </Title>
+      </MiddleSection>
+      <RightSection>{this.props.RightSection}</RightSection>
+    </Wrapper>
+  )
 }
 
 export { TopBar }

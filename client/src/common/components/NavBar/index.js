b44859d0431422a19e61aa544ac4bafdb9edf5bd
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-07-30 13:11:14 -0700

    Add resusable props for nav items

--- client/src/common/components/NavBar/index.js
+++ client/src/common/components/NavBar/index.js
@@ -3,25 +3,21 @@ import home from '../../../assets/img/navIcons/home.png'
 import search from '../../../assets/img/navIcons/search.png'
 import notes from '../../../assets/img/navIcons/notes.png'
 import profile from '../../../assets/img/navIcons/profile.png'
-import {
-  Wrapper,
-  IconContainer,
-  NavIcon
-} from './styled'
+import { Wrapper, IconContainer, NavIcon } from './styled'
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <Wrapper>
-      <IconContainer>
+      <IconContainer onPress={props.navigateHome}>
         <NavIcon source={home} />
       </IconContainer>
-      <IconContainer>
+      <IconContainer onPress={props.navigateToSearch}>
         <NavIcon source={search} />
       </IconContainer>
-      <IconContainer>
+      <IconContainer onPress={props.navigateToNotes}>
         <NavIcon source={notes} />
       </IconContainer>
-      <IconContainer>
+      <IconContainer onPress={props.navigateToProfile}>
         <NavIcon source={profile} />
       </IconContainer>
     </Wrapper>

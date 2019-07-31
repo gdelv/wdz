7c7d3f36f816f16363ab626ee5f3132acbcde565
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-07-31 12:33:53 -0700

    initialize a profile picture for the customer profile page

--- client/src/components/Customer/CustomerProfile/index.js
+++ client/src/components/Customer/CustomerProfile/index.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { ScrollView, Image } from 'react-native'
-import { Wrapper } from './styled'
+import { Wrapper, View_ProfilePicture, Image_ProfilePicture } from './styled'
 import { TopBar, NavBar } from '../../../common'
 import iconGear from '../../../assets/img/iconGear.png'
+import profilePicture from '../../../assets/img/profilePicture.jpeg'
 
 const CustomerProfile = () => {
   renderGearIcon = () => {
     return <Image source={iconGear} />
   }
+  renderProfilePicture = () => {
+    return (
+      <Image
+        source={profilePicture}
+        style={{
+          height: '100%',
+          width: '100%'
+        }}
+      />
+    )
+  }
   return (
     <React.Fragment>
-      <TopBar Title='John Smith' Left={renderGearIcon()} />
       <ScrollView>
+        <TopBar Title='John Smith' Left={renderGearIcon()} />
+        <View_ProfilePicture>{renderProfilePicture()}</View_ProfilePicture>
         {/* PROFILE PICTURE SECTION */}
         {/* Number of FAVS */}
         {/* EMPTY CANVAS SECTION */}
--- client/src/components/Customer/CustomerProfile/styled.js
+++ client/src/components/Customer/CustomerProfile/styled.js
@@ -4,5 +4,13 @@ const Wrapper = styled.View`
   height: 100px;
   background-color: cornflowerblue;
 `
+const View_ProfilePicture = styled.View`
+  height: 200px;
+  /* background-color: seagreen; */
+`
+const Image_ProfilePicture = styled.View`
+  height: 100%;
+  width: 100%;
+`
 
-export { Wrapper }
+export { Wrapper, View_ProfilePicture, Image_ProfilePicture }

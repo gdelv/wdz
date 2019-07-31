c1c1f93f302843173330e3c2e3959827947a598d
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-07-31 12:17:24 -0700

    Add the gear icon on the top left

--- client/src/components/Customer/CustomerProfile/index.js
+++ client/src/components/Customer/CustomerProfile/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, Image } from 'react-native'
 import { Wrapper } from './styled'
 import { TopBar, NavBar } from '../../../common'
+import iconGear from '../../../assets/img/iconGear.png'
 
 const CustomerProfile = () => {
+  renderGearIcon = () => {
+    return <Image source={iconGear} />
+  }
   return (
     <React.Fragment>
-      <TopBar />
+      <TopBar Title='John Smith' Left={renderGearIcon()} />
       <ScrollView />
       <NavBar />
     </React.Fragment>
